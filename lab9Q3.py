c966ee76e4e83a1e14f018ce091b2cb2430f4463
Kennydontsleep <91989012+Kennydontsleep@users.noreply.github.com>
2021-12-05 19:51:00 -0600

    Add files via upload
    These codes or loop code as well as infinite

---
+++ lab9Q3.py
@@ -0,0 +1,11 @@
+number = ['5', '30', '70']
+
+new_number = input("Please enter a number: ")
+
+
+number.append(new_number)
+
+print(number)
+answer = '100'
+while answer == "100":
+    answer = input('Please enter a number:')
